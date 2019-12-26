b172a7c4e43de543a1026f307dd9141903da8bb1
ronsigter <ronchester.mendoza@gmail.com>
2019-12-26 20:16:52 +0800

    Adds function for creating product

--- app/graphql/resolvers/products.rb
+++ app/graphql/resolvers/products.rb
@@ -4,6 +4,15 @@ module Resolvers
       def all_products
         Product.all
       end
+
+      def create_product(**arguments)
+        Product.create(
+          title: arguments[:title],
+          description: arguments[:description],
+          quantity: arguments[:quantity],
+          price: arguments[:price]
+        )
+      end
     end
   end
 end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -11,12 +11,25 @@ module Types
       description "Deletes user token"
     end
 
+    field :create_product, Types::Models::Product, null: false do
+      description "Creates new product"
+
+      argument :title, String, required: true
+      argument :description, String, required: false
+      argument :quantity, Float, required: true
+      argument :price, Float, required: true
+    end
+
     def login(**arguments)
       Resolvers::Users.login(arguments)
     end
 
     def logout()
       Resolvers::Users.logout(context[:current_user])
+    end
+
+    def create_product(**arguments)
+      Resolvers::Products.create_product(arguments)
     end
   end
 end
